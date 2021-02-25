3e74736a67f90f636821ed1a82dfe7bea28cf2d9
Sakshi Santosh Dhaigude <79444689+sakshi72000@users.noreply.github.com>
2021-02-26 00:01:48 +0530

    Create Program6.py

---
+++ Program6.py
@@ -0,0 +1,18 @@
+# Assignment 1
+# Problem 6
+
+def ChkNum(no):
+	if no < 0:
+		print("Number is Negative")
+	elif no > 0:
+		print("Number is Positive")
+	else:
+		print("Number is Zero")
+def main():
+	print("Enter the number:")
+	value = int(input())
+	
+	ChkNum(value)
+
+if __name__ == "__main__":
+	main()
