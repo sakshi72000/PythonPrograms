8f9c887717e76d45e130ae883755fa243c94bb06
Sakshi Santosh Dhaigude <79444689+sakshi72000@users.noreply.github.com>
2021-02-25 23:56:22 +0530

    Create Program3.py

---
+++ Program3.py
@@ -0,0 +1,18 @@
+# Assignment 1
+# Program 3
+
+def Add(no1,no2):
+	return no1 + no2
+
+def main():
+	print("Enter the first number:")
+	value1 = int(input())
+	
+	print("Enter second number:")
+	value2 = int(input())
+	
+	ret = Add(value1,value2)
+	print("The Addition is:",ret)
+
+if __name__ == "__main__":
+	main()
