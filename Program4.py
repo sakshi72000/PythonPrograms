42f705d4fa453762f5e5a011f7079882ad27bec0
Sakshi Santosh Dhaigude <79444689+sakshi72000@users.noreply.github.com>
2021-02-25 23:57:02 +0530

    Create Program4.py

---
+++ Program4.py
@@ -0,0 +1,13 @@
+# Assignment 1
+# Program 4
+
+def Display(no):
+	i = 0
+	for i in range(0,no):
+		print("Marvellous")
+def main():
+	print("Enter how many numbers you want to display")
+	value = int(input())
+	Display(value)
+if __name__ == "__main__":
+	main()
