8c3733ffdadaf1476c15d64b1b6f4c6d0cb96376
Sakshi Santosh Dhaigude <79444689+sakshi72000@users.noreply.github.com>
2021-02-26 00:02:18 +0530

    Create Program7.py

---
+++ Program7.py
@@ -0,0 +1,18 @@
+# Assignment 1
+# Program 7
+
+def Divisible(no):
+	if no % 5 == 0:
+		return True
+	else:
+		return False
+def main():
+	print("Enter the number:")
+	value = int(input())
+	ret = Divisible(value)
+	if ret == True:
+		print("True")
+	else:
+		print("False")
+if __name__ == "__main__":
+	main()
