dd1cbc91f6f8ee2d18d3ace29bd74353a9cbee99
Sakshi Santosh Dhaigude <79444689+sakshi72000@users.noreply.github.com>
2021-02-26 00:04:00 +0530

    Create Program10.py

---
+++ Program10.py
@@ -0,0 +1,14 @@
+# Assignment 1
+# Program 10
+
+def Name(nam):
+	len(nam)
+	print(len(nam))
+
+def main():
+	print("Enter the name")
+	name = str(input())
+	Name(name)
+
+if __name__ == "__main__":
+	main()
